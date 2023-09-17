2f3fbc93bab1e58f34db26a40ff79f6ec208268a
maicol hernandez <maykolandres4@gmail.com>
2023-09-17 07:44:22 -0500

    created first migration to SQL server

---
+++ Data/SelidaDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using selida.Models.Domain;
+
+namespace selida.Data
+{
+    public class SelidaDbContext : DbContext
+    {
+        public SelidaDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using selida.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<SelidaDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("selidaDb")));
 
 var app = builder.Build();
 
