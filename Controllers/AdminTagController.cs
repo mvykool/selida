46595ae57e3c8d923760f551317f755da8bfd846
maicol hernandez <maykolandres4@gmail.com>
2023-09-17 09:30:27 -0500

    created tag table, synced with DB

---
+++ Controllers/AdminTagController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using selida.Data;
+using selida.Models.Domain;
+using selida.Models.ViewModels;
+
+namespace selida.Controllers
+{
+    public class AdminTagController : Controller
+    {
+        private readonly SelidaDbContext selidaDbContext;
+
+        public AdminTagController(SelidaDbContext selidaDbContext)
+        {
+            this.selidaDbContext = selidaDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.displayName
+            };
+
+            selidaDbContext.Tags.Add(tag);
+            selidaDbContext.SaveChanges();
+
+            return View("Add");
+        }
+    }
+}
---
+++ Models/ViewModels/AddTagRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace selida.Models.ViewModels
+{
+    public class AddTagRequest
+    {
+        public string Name { get; set; }
+
+        public string displayName { get; set; }
+    }
+}
