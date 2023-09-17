40ff7734f2b2794e4b8f9fbc0c05c1188fe21f78
maicol hernandez <maykolandres4@gmail.com>
2023-09-17 17:27:02 -0500

    created blogPost adding page

---
+++ Controllers/AdminBlogController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using selida.Data;
+using selida.Models.Domain;
+using selida.Models.ViewModels;
+
+namespace selida.Controllers
+{
+    public class AdminBlogController : Controller
+    {
+        private readonly SelidaDbContext selidaDbContext;
+
+        public AdminBlogController(SelidaDbContext selidaDbContext)
+        {
+            this.selidaDbContext = selidaDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult Add(AddBlogRequest addBlogRequest)
+        {
+            var blog = new BlogPost
+            {
+                Heading = addBlogRequest.Heading,
+                PageTitle = addBlogRequest.PageTitle,
+                Content = addBlogRequest.Content,
+                ShortDescription = addBlogRequest.ShortDescription,
+                FeatureImageUrl = addBlogRequest.FeatureImageUrl,
+                PublishDate = addBlogRequest.PublishDate,
+                Author = addBlogRequest.Author,
+            };
+
+            selidaDbContext.BlogPosts.Add(blog);
+            selidaDbContext.SaveChanges();
+
+            return View("Add");
+        }
+    }
+}
---
+++ Models/ViewModels/AddBlogRequest.cs
@@ -0,0 +1,19 @@
+﻿namespace selida.Models.ViewModels
+{
+    public class AddBlogRequest
+    {
+        public string Heading { get; set; }
+
+        public string PageTitle { get; set; }
+
+        public string Content { get; set; }
+
+        public string ShortDescription { get; set; }
+
+        public string FeatureImageUrl { get; set; }
+
+        public DateTime PublishDate { get; set; }
+
+        public string Author { get; set; }
+    }
+}
