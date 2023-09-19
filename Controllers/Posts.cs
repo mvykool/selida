882cbc1e00cebfeaf59f4b561f4e9a8fad673daa
maicol hernandez <maykolandres4@gmail.com>
2023-09-19 06:38:19 -0500

    created controller to details view

--- Controllers/Posts.cs
+++ Controllers/Posts.cs
@@ -22,5 +22,20 @@ namespace selida.Controllers
             return View(posts);
   
         }
+
+        [HttpGet]
+        // GET: Posts with Id
+        public ActionResult Details(Guid id)
+        {
+            var post = selidaDbContext.BlogPosts.FirstOrDefault(p => p.Id == id);
+
+            if (post == null)
+            {
+                return NotFound(); // Return a 404 Not Found response if the post doesn't exist.
+            }
+
+            return View(post);
+        }
+
     }
 }
